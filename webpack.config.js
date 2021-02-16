7c157ca5545824167b9aa7878993553446e00bff
bbumjun <bumjun7928@gmail.com>
2021-02-16 23:58:10 +0900

    chore: add vendor prefix in css with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader']
+                use: [MiniCssExtractPlugin.loader,'css-loader','postcss-loader']
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
