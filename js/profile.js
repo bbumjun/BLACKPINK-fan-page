68de6359e700b8ce74576f14ed75556d0dfd9aa5
bbumjun <bumjun7928@gmail.com>
2021-02-27 23:31:36 +0900

    style: 프로필이 화면 노출 시 텍스트 화면에 나타나도록 스타일 적용

--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,29 @@
-import '../scss/profile/profile.scss'
-import './common'
+import "../scss/profile/profile.scss";
+import "./common";
 require.context("../images/common", true);
 require.context("../images/profile", true);
 
-document.addEventListener('DOMContentLoaded',()=>{
-    
-})
+window.addEventListener("load", ()=>{
+  const options = {
+    threshold: 0.8,
+  };
+  const io = new IntersectionObserver((entries, observer)=>{
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      console.log(entry.target);
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    });
+  }, options);
+  const names = document.querySelectorAll(".member .name");
+  const details = document.querySelectorAll(".member .detail");
+  names.forEach((name)=>{
+    io.observe(name);
+  });
+  details.forEach((detail)=>{
+    io.observe(detail);
+  });
+});
 
