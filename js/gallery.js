889f8dae4ebedb0da84bbf1c6605d869112aa3ae
bbumjun <bumjun7928@gmail.com>
2021-03-03 02:48:57 +0900

    갤러리의 각 이미지가 리사이즈 된이후에 화면에 보이도록 수정

--- js/gallery.js
+++ js/gallery.js
@@ -6,21 +6,21 @@ require.context("../images/gallery",true)
 
 
 function resizeGridItems(){
-    const items = document.querySelectorAll('.content')
+    const items = document.querySelectorAll('.item')
     items.forEach(item=>{
         imagesLoaded(item,(instance)=>{
             const item = instance.elements[0];
             const grid = document.querySelector('.wrapper')
             const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'))
             const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'))
-            const rowSpan =  Math.ceil((item.offsetHeight+rowGap)/(rowHeight+rowGap))
-            item.style.gridRowEnd = "span "+rowSpan;
+            const rowSpan =  Math.floor((item.querySelector('.content').offsetHeight+rowGap)/(rowHeight+rowGap))
+            item.style.gridRowEnd = "span "+ rowSpan;
+            item.style.visibility = 'visible';
         })
     })
 }
 
 window.addEventListener('load',()=>{
-
     resizeGridItems()
 })
 
