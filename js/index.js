abf6941b4c678f7577cd384be019da7b13f64c8f
bbumjun <bumjun7928@gmail.com>
2021-03-03 02:12:31 +0900

    index 페이지 backgorund image가 완전히 로드된후 보이도록 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 import "../scss/index/index.scss";
 import "./common";
+import imagesLoaded from 'imagesloaded'
 require.context("../images/common", true);
 require.context("../images/index", true);
 
+(function loadBgImg (){
+    let bgImg = new Image()
+    bgImg.src = '../../images/index/background.jpg'
+    bgImg.onload = ()=>{
+        document.querySelector('.background')
+        .style
+        .backgroundImage = `url(${bgImg.src})`
+    }
+})()
