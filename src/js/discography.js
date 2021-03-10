f837615a44f0b703d2f8c2c0ebf7890149642432
bbumjun <bumjun7928@gmail.com>
2021-03-10 22:57:00 +0900

    [Refactor] 정보 fetch 후 스크롤하도록 수정

--- src/js/discography.js
+++ src/js/discography.js
@@ -1,25 +1,28 @@
 import "./common";
 import "../scss/discography/discography.scss";
+import {serverUrl} from './config'
 require.context("../images/common", true);
 require.context("../images/discography", true);
 
-const albumList = document.querySelector('#album-list')
-albumList.addEventListener('click',({target})=>{
-    const container = document.querySelector('#album-info')
-    container.style.height = '100%';
-
-    if(target.dataset.id){
-        fetch(`http://localhost:3001/gallery/album-info/${target.dataset.id}`)
-        .then(res=>res.json())
-        .then(({albumInfo})=>{
-            const title = container.querySelector('.album-info__detail__container__description__title')
-            const description =container.querySelector('.album-info__detail__container__description__text')            
-            const coverImage = container.querySelector('.album-info__detail__cover img')
-            const video = container.querySelector('.album-info__detail__container__video iframe')
-            coverImage.src = albumInfo.coverImageSrc
-            video.src = albumInfo.videoSrc
-            title.textContent = albumInfo.title
-            description.textContent = albumInfo.text
-        })
-    }
+window.addEventListener('load',()=>{
+        const albumList = document.querySelector('#album-list')
+        albumList.addEventListener('click',({target})=>{
+            const container = document.querySelector('#album-info')
+            container.style.display = 'block';
+            if(target.dataset.id){
+                fetch(`${serverUrl}/gallery/album-info/${target.dataset.id}`)   
+                .then(res=>res.json())
+                .then(({albumInfo})=>{
+                const coverImage = container.querySelector('.album-info__detail__cover img')
+                const title = container.querySelector('.album-info__detail__container__description__title')
+                const description =container.querySelector('.album-info__detail__container__description__text')            
+                const video = container.querySelector('.album-info__detail__container__video iframe')
+                coverImage.src = albumInfo.coverImageSrc
+                video.src = albumInfo.videoSrc
+                title.textContent = albumInfo.title
+                description.textContent = albumInfo.text
+                window.scrollTo({top:container.getBoundingClientRect().top,behavior:'smooth'})
+            })
+        }
+    })
 })
