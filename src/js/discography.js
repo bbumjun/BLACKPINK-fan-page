4f45f3829d476aa4915b99015b07a3d89abd0e15
bbumjun <bumjun7928@gmail.com>
2021-03-10 18:53:32 +0900

    [add] 앨범 이미지 클릭시 앨범정보 fetch 후 나타내는 기능 추가

--- src/js/discography.js
+++ src/js/discography.js
@@ -2,3 +2,24 @@ import "./common";
 import "../scss/discography/discography.scss";
 require.context("../images/common", true);
 require.context("../images/discography", true);
+
+const albumList = document.querySelector('#album-list')
+albumList.addEventListener('click',({target})=>{
+    const container = document.querySelector('#album-info')
+    container.style.height = '100%';
+
+    if(target.dataset.id){
+        fetch(`http://localhost:3001/gallery/album-info/${target.dataset.id}`)
+        .then(res=>res.json())
+        .then(({albumInfo})=>{
+            const title = container.querySelector('.album-info__detail__container__description__title')
+            const description =container.querySelector('.album-info__detail__container__description__text')            
+            const coverImage = container.querySelector('.album-info__detail__cover img')
+            const video = container.querySelector('.album-info__detail__container__video iframe')
+            coverImage.src = albumInfo.coverImageSrc
+            video.src = albumInfo.videoSrc
+            title.textContent = albumInfo.title
+            description.textContent = albumInfo.text
+        })
+    }
+})
